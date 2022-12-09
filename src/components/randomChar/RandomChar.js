03cc468f9516ac590e2656bef3325f3cefcaf851
IrinaBreus <irabreus@gmail.com>
2022-12-09 11:08:58 +0300

    add rendom char

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,22 +1,51 @@
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
+import { useEffect, useState } from 'react';
+import MarvelService from '../../services/MarvelService';
 
 const RandomChar = () => {
+    
+    const [state, setState] = useState({});
+
+    const marvelService = new MarvelService();
+
+    const updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        marvelService
+            .getCharacter(id)
+            .then(res => {
+                setState(res);
+            })
+    }
+
+    useEffect(() => {
+        updateChar();
+    }, [])
+
+    const {name, description, thumbnail, homepage, wiki} = state;
+
+    console.log(description);
+
     return (
         <div className="randomchar">
             <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
+                <img src={thumbnail} alt="Random character" className="randomchar__img"/>
                 <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
+                    <p className="randomchar__name">{name}</p>
                     <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+                        {description}
                     </p>
                     <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
+                        <a href={homepage} 
+                            className="button button__main"
+                            target='blank'
+                        >
                             <div className="inner">homepage</div>
                         </a>
-                        <a href="#" className="button button__secondary">
+                        <a href={wiki} 
+                            className="button button__secondary"
+                            target='blank'
+                        >
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
